3fd79344fffa3293b9da485dd8d19e86b0911fb2
Burcu Uğur <burcuturan737@gmail.com>
2022-10-11 22:51:27 +0300

    Group2 Projects have been created.

---
+++ src/day07/Variables.java
@@ -0,0 +1,17 @@
+package day07;
+
+public class Variables {
+    public static void main(String[] args) {
+        //= operatörü (assignment operatörü) sağında bulunan sabit bir değer (literal) veya bir işlemin sonucunu
+        // solundaki değişkenin (variable) temsil ettiği hafıza bölgesine kaydeder.
+        int yas=40; //bu satır bir java ifadesidir (statementtir).
+
+
+
+        //Karenin alanını ve çevresini hesaplayınız.
+        int kenar=5;
+        System.out.println(kenar*kenar);
+        System.out.println(4*kenar);
+
+    }
+}
