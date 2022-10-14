5da16ee721c69676cc7ea2a05138b425fcc91c38
Burcu Uğur <burcuturan737@gmail.com>
2022-10-14 22:55:23 +0300

    Day08 tamamlandı.

---
+++ src/day08/Task7.java
@@ -0,0 +1,16 @@
+package day08;
+
+public class Task7 {
+    public static void main(String[] args) {
+        int deger=10;
+        int deger2=20;
+        float deger3=12.5F;
+        String metin="Java programming";
+        System.out.println(deger);
+        System.out.println(deger2);
+        System.out.println(deger3);
+        System.out.println(metin);
+
+    }
+
+}
---
+++ src/day08/Task8.java
@@ -0,0 +1,20 @@
+package day08;
+
+public class Task8 {
+    public static void main(String[] args) {
+        /*int year =1491;
+        String metin= "In "+year+" columbus sailed the oceanblue";
+        year =1492;
+        String metin2= "In "+year+" columbus sailed the oceanblue";
+
+        System.out.println(metin);
+        System.out.println(metin2);*/
+
+        int year=1491;
+        System.out.printf("IN %d COLUMBUS SAILED THE OCEANBLUE", year);
+        System.out.println();
+        year=1492;
+        System.out.printf("IN %d COLUMBUS SAILED THE OCEANBLUE", year);
+
+    }
+}
