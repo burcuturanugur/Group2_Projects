ad8df7536afb81da77994571e302e1f8d7b22d0c
Burcu Uğur <burcuturan737@gmail.com>
2022-10-25 19:57:28 +0300

    Day 014

--- src/day014/Task2.java
+++ src/day014/Task2.java
@@ -8,7 +8,7 @@ public class Task2 {
             Daha sonra iki değişkenin değerlerini yer değiştirip ekrana tekrar yazdırınız.
          */
 
-    int tamsayi1=5;
+   /* int tamsayi1=5;
     int tamsayi2=10;
         System.out.println("1. sayı " +tamsayi1 + " ve 2. sayı " +tamsayi2);
 
@@ -16,9 +16,19 @@ public class Task2 {
     tamsayi1=tamsayi2;
     tamsayi2=tamsayi;
         System.out.println("1. sayı " +tamsayi1 + " ve 2. sayı " + tamsayi2);
+*/
 
+    int x=7;
+    int y=5;
+        System.out.printf("x = %d\n" , x);
+        System.out.printf("y = %d\n" , y);
+
+        int z=x;
+        x=y;
+        y=z;
+        System.out.printf("x= %d\n" , x);
+        System.out.printf("y= %d\n" , y);
 
     }
-
 
 }
