b27d22ff9ccf842774633ec01875c7f4aa62690a
burcuturanugur <107644861+burcuturanugur@users.noreply.github.com>
2022-11-14 23:46:48 +0300

    For Loops

--- src/day017/Task2.java
+++ src/day017/Task2.java
@@ -3,19 +3,19 @@ package day017;
 public class Task2 {
     public static void main(String[] args) {
 
-       /* for (int i = 1; i <= 10; i++) {
+    /*    for (int i = 1; i <= 10; i++) {
             int sayi=i*i;
             System.out.println(i+ "--->" + sayi );
-        }*/
-
+        }
+*/
         //2. yöntem
 
-        System.out.println("Number\tSquared");
+     /*   System.out.println("Number\tSquared");
 
         for (int i = 1; i <= 10; i++) {
             System.out.printf("%5d\t\t%5d" ,i,i*i );
             System.out.println();
-        }
+        }*/
 
     }
 }
