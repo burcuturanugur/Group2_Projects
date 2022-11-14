a861999660da6baf5c7a3ef03132e2ddec9c1a39
burcuturanugur <107644861+burcuturanugur@users.noreply.github.com>
2022-11-14 23:50:00 +0300

    Methods

---
+++ src/day025/hayvanlar/Aslan.java
@@ -0,0 +1,14 @@
+package day025.hayvanlar;
+
+public class Aslan {
+    public double boyu;
+    public double kilosu;
+    public String turu;
+    public String cinsiyeti;
+    public String rengi;
+
+
+
+    //Constructor
+
+}
