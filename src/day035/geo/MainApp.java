a2ba06dfeee2129be42633278a0dbd9fe90c8f21
burcuturanugur <107644861+burcuturanugur@users.noreply.github.com>
2022-11-24 23:35:29 +0300

    OOP - Polymorphism - interface

--- src/day035/geo/MainApp.java
+++ src/day035/geo/MainApp.java
@@ -20,6 +20,16 @@ public class MainApp {
         kare1.setLocation(120,20);
         pencere.add(kare1);
 
+        JLabel lblAlan=new JLabel("Alan: " +kare1.getAlan());
+        lblAlan.setLocation(330,20);
+        lblAlan.setSize(200,30);
+        pencere.add(lblAlan);
+
+        JLabel lblCevre=new JLabel("Çevre: " +kare1.getCevre());
+        lblCevre.setLocation(330,60);
+        lblCevre.setSize(200,30);
+        pencere.add(lblCevre);
+
 
 
 
---
+++ src/day035/geo/MainApp2.java
@@ -0,0 +1,7 @@
+package day035.geo;
+
+public class MainApp2 {
+    public static void main(String[] args) {
+        new Pencere("Geo V2.0");
+    }
+}
